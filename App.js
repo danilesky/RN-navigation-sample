60aa5d7767d140dd2b30ce8f3287ae8fde7dfe64
Daniel Skysľak <danilesky17@gmail.com>
2022-09-28 21:58:08 +0200

    Navigator and Screen init

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { AppArea } from "./src/components/AppArea.component";
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <AppArea>
-      <Text>Open up App.js to start working on your app!</Text>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
     </AppArea>
   );
