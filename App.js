92f9de71bbcb0c9760851cfbd5e926a17668904e
Daniel Skysľak <danilesky17@gmail.com>
2022-09-28 22:52:46 +0200

    Adding icons to tabBar navigation

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { AppArea } from "./src/components/utils/AppArea.component";
 import { About } from "./src/features/screens/About.screen";
 import { Contact } from "./src/features/screens/Contact.screen";
 import { Home } from "./src/features/screens/Home.screen";
+import { Feather } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,7 +13,21 @@ export default function App() {
   return (
     <AppArea>
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: () => {
+              let iconName;
+              if (route.name === "Home") {
+                iconName = "home";
+              } else if (route.name === "About") {
+                iconName = "book-open";
+              } else if (route.name === "Contact") {
+                iconName = "mail";
+              }
+              return <Feather name={iconName} size={24} color="black" />;
+            },
+          })}
+        >
           <Tab.Screen name="Home" component={Home} />
           <Tab.Screen name="About" component={About} />
           <Tab.Screen name="Contact" component={Contact} />
